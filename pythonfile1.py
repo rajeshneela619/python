67a0e181ce5029bf557a2eaa9de48cccd6a22b60
rajeshneela619 <129382028+rajeshneela619@users.noreply.github.com>
2023-06-25 12:22:37 +0530

    s

--- pythonfile1.py
+++ pythonfile1.py
@@ -1,4 +1,11 @@
-$ python calculate.py 
-    input your name: XYZ
-    input your age: 33 
-    XYZ's age is 33 years or 406 months or 12328 days
+# Python3 code to calculate age in years
+from datetime import date
+
+def calculateAge(birthDate):
+	days_in_year = 365.2425
+	age = int((date.today() - birthDate).days / days_in_year)
+	return age
+		
+# Driver code
+print(calculateAge(date(1997, 2, 3)), "years")
+
